7027283fa50a032ff4136599426b307425cf85b8
juha <juha.majuri@tieto.com>
2017-03-30 22:58:07 +0300

    Initial commit.

---
+++ src/game_of_life/gol.clj
@@ -0,0 +1,76 @@
+(ns game-of-life.gol
+  (:gen-class))
+
+(def dead 0)
+(def alive 1)
+
+(defn init-cell []
+  (rand-int 2))
+
+(defn next-state
+  [cell neighbours]
+  (case neighbours
+    0 dead
+    1 dead
+    2 cell
+    3 alive
+    dead))
+
+(defn get-at [board x y]
+  (try
+    (nth (nth board y) x)
+    (catch IndexOutOfBoundsException e 0)))
+
+(defn get-neighbour-values [board x y]
+  (+
+   (get-at board (- x 1) (- y 1))
+   (get-at board x (- y 1))
+   (get-at board (+ x 1) (- y 1))
+   (get-at board (- x 1) y)
+   (get-at board (+ x 1) y)
+   (get-at board (- x 1) (+ y 1))
+   (get-at board x (+ y 1))
+   (get-at board (+ x 1) (+ y 1))))
+
+(defn next-cell-value [board x y]
+  (next-state (get-at board x y)
+              (get-neighbour-values board x y)))
+
+(defn update-board [board]
+  (let [height (count board)
+        width (count (nth board 0))]
+    (loop [result (transient [])
+           y 0]
+      (if (= y height)
+        (persistent! result)
+        (recur (conj! result
+                      (map (fn [x] (next-cell-value board x y)) (vec (range 0 width))))
+               (inc y))))))
+
+(defn create-board
+  [max-x max-y]
+  (loop [result (transient [])
+         y 0]
+    (if (<= max-y y)
+      (persistent! result)
+      (recur (conj! result (vec (repeatedly max-x #(init-cell))))
+             (inc y)))))
+
+(defn still-alive? [board]
+  (< 0 (reduce + (flatten board))))
+
+(defn print-board
+  [board]
+  (doseq [row board]
+    (doseq [cell row] (print (if (= cell alive) "*" " ") " "))
+    (println)))
+
+(defn board-iter [board tics]
+  (print-board board)
+  (Thread/sleep 1000)
+  (if (or (not (still-alive? board)) (= tics 0))
+   tics
+    (recur (update-board board) (dec tics))))
+
+(defn -main []
+  (board-iter (create-board 20 20) 50))
---
+++ test/game_of_life/game_of_life_test.clj
@@ -0,0 +1,7 @@
+(ns nhl-crawler.core-test
+  (:require [clojure.test :refer :all]
+            [nhl-crawler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
